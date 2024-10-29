444deccb5533c0fc9453e59fe9ca47c5d6e8373d
MarianoVIsabella <marianovisabella@libero.it>
2024-10-29 16:24:58 +0100

    Aggiunta la validazione del form in contacts.html via regex

--- src/main/resources/static/js/validazione.js
+++ src/main/resources/static/js/validazione.js
@@ -1,1 +1,16 @@
-console.log("Ora scrivo lo javascript per validare gli utenti");
+function validate(event){
+    event.preventDefault();
+    firstName = document.getElementById("fname");
+    lastName= document.getElementById("lname");
+    nome = firstName.value;
+    cognome= lastName.value;
+    //la mail è già controllata perché il tipo di input field è "mail", eventuali controlli superflui
+    const pattern=/^[A-Za-z]+(\s[A-Za-z]+)*$/
+    if (! pattern.test(nome) || ! pattern.test(cognome)){
+        alert("Nome e/o cognome non validi, riprovare");
+    }
+}
+window.addEventListener("load", function(){
+    form = document.getElementById("contactForm");
+    form.addEventListener("submit", validate);
+});
