0f8ec864ce775bf2e18638d391cfcd0c899d1f8e
MarianoVIsabella <marianovisabella@libero.it>
2024-11-05 17:10:27 +0100

    Aggiunta finalmente la possibilità di inserire annate multiple di vittoria + aggiornati todo

--- src/main/resources/static/js/addTrophy.js
+++ src/main/resources/static/js/addTrophy.js
@@ -7,8 +7,11 @@ document.getElementById("newTrophyForm").addEventListener("submit", function(eve
     event.preventDefault();
     const competition = document.getElementById("competition").value;
     const year = document.getElementById("year").value;
+    const array_years=year.split(",").map(s => s.trim());
+    //divido la striga per carattere ",", e in ognuna rimuovo eventuali spazi indesiderati su ogni elemento s dell'array
     const tableBody = document.getElementById("trophyTable").getElementsByTagName('tbody')[0];
-    addNewRow(tableBody ,competition , year); //aggiunta della nuova riga
+    //addNewRow(tableBody ,competition , year); //aggiunta della nuova riga
+    addNewRowArray(tableBody,competition,array_years);
     //nascondo nuovamente il form e ne azzero i campi
     document.getElementById("trophyForm").style.display = "none";
     document.getElementById("newTrophyForm").reset();
@@ -25,4 +28,18 @@ function addNewRow(tableBody , name, year){
     yearCell.innerHTML = `<span class="badge bg-primary">${year}</span>`;
     timesCell.textContent="1";
     // console.log('Adding new Row')
+}
+function addNewRowArray(tableBody, name, years){
+    const newRow = tableBody.insertRow();
+    const nameCell = newRow.insertCell(0);
+    const yearCell = newRow.insertCell(1);
+    const timesCell= newRow.insertCell(2);
+
+    nameCell.textContent = name;
+    yearCell.innerHTML= years.map( year => `<span class="badge bg-primary">${year}</span> `).join("")
+    //questa funzione crea un array contenente tanti span class con il rispettivo valore di year, e poi le unisce in un'unica
+    //stringa via join per passarla a innerHTML
+    //yearCell.innerHTML = `<span class="badge bg-primary">${years[0]}</span>`;
+    volteVittoria=years.length
+    timesCell.textContent=volteVittoria;
 }
