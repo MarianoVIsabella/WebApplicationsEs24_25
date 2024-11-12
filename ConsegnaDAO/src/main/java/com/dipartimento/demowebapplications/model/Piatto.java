4bc9ad8c197396ca42cc1d0605fc5f3b9e507186
MarianoVIsabella <marianovisabella@libero.it>
2024-11-12 14:50:54 +0100

    Consegna DAO fix

---
+++ ConsegnaDAO/src/main/java/com/dipartimento/demowebapplications/model/Piatto.java
@@ -0,0 +1,42 @@
+package com.dipartimento.demowebapplications.model;
+
+import lombok.*;
+
+import java.util.List;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Piatto {
+
+    protected String nome;
+    protected String ingredienti;
+    protected List<Ristorante> ristoranti;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIngredienti() {
+        return ingredienti;
+    }
+
+    public void setIngredienti(String ingredienti) {
+        this.ingredienti = ingredienti;
+    }
+
+    public List<Ristorante> getRistoranti() {
+        return ristoranti;
+    }
+
+    public void setRistoranti(List<Ristorante> ristoranti) {
+        this.ristoranti = ristoranti;
+    }
+}
---
+++ ConsegnaDAO/src/main/java/com/dipartimento/demowebapplications/persistence/dao/impljdbc/PiattoDaoJDBC.java
@@ -0,0 +1,154 @@
+package com.dipartimento.demowebapplications.persistence.dao.impljdbc;
+
+import com.dipartimento.demowebapplications.model.Piatto;
+import com.dipartimento.demowebapplications.model.Ristorante;
+import com.dipartimento.demowebapplications.persistence.DBManager;
+import com.dipartimento.demowebapplications.persistence.dao.PiattoDao;
+import com.dipartimento.demowebapplications.persistence.dao.RistoranteDao;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PiattoDaoJDBC implements PiattoDao {
+
+    Connection connection;
+
+
+    public PiattoDaoJDBC(Connection conn){
+        this.connection = conn;
+    }
+
+    @Override
+    public List<Piatto> findAll() {
+        List<Piatto> piatti = new ArrayList<Piatto>();
+        String query = "select * from piatto";
+        Statement st = null;
+        try {
+            st = connection.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()){
+                Piatto piatto = new PiattoProxy();
+
+                piatto.setNome(rs.getString("nome"));
+                piatto.setIngredienti(rs.getString("ingredienti"));
+
+                piatti.add(piatto);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return piatti;
+    }
+
+    @Override
+    public Piatto findByPrimaryKey(String nome) {
+        String query = "SELECT nome, ingredienti FROM piatto WHERE nome = ?";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, nome);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                Piatto piatto = new PiattoProxy();
+                piatto.setNome(resultSet.getString("nome"));
+                piatto.setIngredienti(resultSet.getString("ingredienti"));
+                return piatto;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+
+    @Override
+    public void save(Piatto piatto) {
+        /*
+        INSERT INTO t VALUES (1,'foo updated'),(3,'new record')
+ON CONFLICT (id) DO UPDATE SET txt = EXCLUDED.txt;
+         */
+        String query = "INSERT INTO piatto (nome, ingredienti) VALUES (?, ?) " +
+                "ON CONFLICT (nome) DO UPDATE SET ingredienti = EXCLUDED.ingredienti";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, piatto.getNome());
+            statement.setString(2, piatto.getIngredienti());
+            statement.executeUpdate();
+            List<Ristorante> ristoranti= piatto.getRistoranti();
+            if (ristoranti==null || ristoranti.isEmpty()){
+                return;
+            }
+            //resetto tutte le tuple che legano il piatto che sto salvando a un ristorante per poterle
+            //reinserire
+            resetRelationsPresentInTheJoinTable(connection,piatto.getNome());
+            RistoranteDao rd=DBManager.getInstance().getRistoranteDao();
+            for (Ristorante r : ristoranti){
+                rd.save(r);
+                insertJoinRistorantePiatto(connection,r.getNome(),piatto.getNome());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void resetRelationsPresentInTheJoinTable(Connection connection, String nomePiatto) throws Exception {
+
+        String query="Delete FROM ristorante_piatto WHERE piatto_nome= ? ";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, nomePiatto);
+        preparedStatement.execute();
+    }
+
+    private void insertJoinRistorantePiatto(Connection connection , String nomeRistorante, String nomePiatto) throws SQLException {
+
+        String query="INSERT INTO ristorante_piatto (ristorante_nome,piatto_nome) VALUES (? , ?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, nomeRistorante);
+        preparedStatement.setString(2, nomePiatto);
+        preparedStatement.execute();
+
+    }
+
+
+    @Override
+    public void delete(Piatto piatto) {
+
+    }
+
+    @Override
+    public List<Piatto> findAllByRistoranteName(String ristoranteNome) {
+
+
+
+        List<Piatto> piatti = new ArrayList<>();
+        String query = "SELECT p.nome, p.ingredienti FROM piatto p " +
+                "JOIN ristorante_piatto rp ON p.nome = rp.piatto_nome " +
+                "WHERE rp.ristorante_nome = ?";
+
+        System.out.println("going to execute:"+query);
+
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, ristoranteNome);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                String nome = resultSet.getString("nome");
+                String ingredienti = resultSet.getString("ingredienti");
+                piatti.add(new Piatto(nome, ingredienti,null));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return piatti;
+    }
+
+    public static void main(String[] args) {
+        PiattoDao piattoDao = DBManager.getInstance().getPiattoDao();
+        List<Piatto> piatti = piattoDao.findAll();
+        for (Piatto piatto : piatti) {
+            System.out.println(piatto.getNome());
+            System.out.println(piatto.getIngredienti());
+
+        }
+    }
+}
